ede2f91c5c20523dd40ea60c4315478f5c5f0f4a
AbdullaAlHarun <abdhar01276@stud.noroff.no>
2025-01-27 17:36:20 +0100

    Added frontend

--- src/index.js
+++ src/index.js
@@ -24,12 +24,12 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', allowedOrigins[0]);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.sendStatus(200);
-});
+//app.options('*', (req, res) => {
+ // res.header('Access-Control-Allow-Origin', allowedOrigins[0]);
+ // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+ // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//  res.sendStatus(200);
+//});
 
 app.get('/', (req, res) => {
   res.send('Server is running');
